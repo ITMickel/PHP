3498d4517916f63d27f5ab11f585a208e4c0ef38
ITMickel <121444047+ITMickel@users.noreply.github.com>
2023-07-20 21:54:07 +0200

    Add files via upload

---
+++ pętle.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>pętle</title>
+</head>
+<body>
+<?php
+$numer = 37;
+
+if ($numer < 0 or $number > 20) {
+
+echo "Liczna $number jest ujemna lub większa niż 20.<br>";
+}
+
+elseif ($number != 3.5) {
+
+echo "Liczna $number nie jest równa 3.5<br>";
+}
+
+else {
+
+echo "Liczna $number jest równa 3.5.";
+}
+?>
+</body>
+</html>
